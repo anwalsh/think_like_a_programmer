a8cd7e9c4b161ec4b9997d8c851b7f02d7c6e859
Andy Walsh <andyjasonwalsh@gmail.com>
2019-05-14 22:18:22 -0500

    Added framework for the Decoding Message problem

---
+++ decode_message/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    return;
+}
+
+fn upper_case_mode() {
+    // TODO: input % 27 returns a integer associated with the nth character of the alphabet
+}
+
+fn lower_case_mode() {
+    // TODO: the same as the above but with lowercase letters.
+}
+
+fn punctuation_mode() {
+    // TODO: input % 9 returns an integer associated with a symbol:
+    // 1=!
+    // 2=?
+    // 3=,
+    // 4=.
+    // 5=SPC
+    // 6=;
+    // 7="
+    // 8='
+}
+
+fn decode() {
+    // TODO: main function to handle decoding a message. This function will switch on 0 to the
+    // following mode.
+}
+
+fn parse_input() {
+    // TODO: parse user input from the CLI
+}

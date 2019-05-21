d8819fe9ec7d6318e7f01343af63a8676e20a965
Andy Walsh <andyjasonwalsh@gmail.com>
2019-05-20 23:30:20 -0500

    Implemented tests for three functions and implemented functions
    Implemented uppercase_mode(), lowercase_mode(), and punctuation_mode() as well
    as the respective tests for each

--- decode_message/src/main.rs
+++ decode_message/src/main.rs
@@ -9,17 +9,22 @@ enum Mode {
 fn main() {
     let x = 097;
     println!("{:?}", x.to_ascii_char());
+    println!("{}", punctuation_mode(1));
     return;
 }
 
-fn upper_case_mode(code_digit: u8) -> char {
+fn uppercase_mode(code_digit: u32) -> ascii::AsciiChar {
     // TODO: input % 27 returns a integer associated with the nth character of the alphabet
-    return 'a';
+    let digit = code_digit % 27;
+    let value = ToAsciiChar::to_ascii_char((digit as u8 % 27) + 64).unwrap();
+    return value;
 }
 
-fn lower_case_mode(code_digit: u8) -> char {
+fn lowercase_mode(code_digit: u32) -> ascii::AsciiChar {
     // TODO: the same as the above but with lowercase letters.
-    return 'a';
+    let digit = code_digit % 27;
+    let value = ToAsciiChar::to_ascii_char((digit as u8 % 27) + 96).unwrap();
+    return value;
 }
 
 fn punctuation_mode(code_digit: u8) -> char {
@@ -44,7 +49,7 @@ fn punctuation_mode(code_digit: u8) -> char {
         6 => output = ';',
         7 => output = '"',
         8 => output = '\'',
-        _ => panic!("Failed to parse!"),
+        _ => panic!("Failed to parse {} . . .", input),
     }
     return output;
 }
@@ -52,8 +57,42 @@ fn punctuation_mode(code_digit: u8) -> char {
 fn decode() {
     // TODO: main function to handle decoding a message. This function will switch on 0 to the
     // following mode.
+    // User the Mode enum I am hoping to swap based on input.
 }
 
 fn parse_input() {
     // TODO: parse user input from the CLI
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn uppercase_mode_test() {
+        let x = 18;
+        assert_eq!('R', uppercase_mode(x));
+        let y = 241;
+        assert_eq!('Y', uppercase_mode(y));
+    }
+
+    #[test]
+    fn lowercase_mode_test() {
+        let x = 171;
+        assert_eq!('i', lowercase_mode(x));
+        let y = 20620;
+        assert_eq!('s', lowercase_mode(y));
+    }
+
+    #[test]
+    fn punctuation_mode_test() {
+        let x = 1;
+        assert_eq!('!', punctuation_mode(x));
+        let y = 6;
+        assert_eq!(';', punctuation_mode(y));
+    }
+
+    fn decode_test() {}
+
+    fn parse_input_test() {}
 }

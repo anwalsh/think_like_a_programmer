8388b57e2b722ae6a7e6c478a8cf1c18366f1bca
Andy Walsh <andyjasonwalsh@gmail.com>
2019-05-23 16:04:00 -0500

    Implemented test for decode() and began structuring decode()
    - Added the debug trait to Mode
    - Added static MODE variable to track mode
    - Updated function signature for punctuation_mode()
    - Created a match for acting based on the mode
    - Transitioned input match in punctuation_mode() to ASCII
    - WIP decode() implementation
    - decode_test() implemented

--- decode_message/src/main.rs
+++ decode_message/src/main.rs
@@ -1,33 +1,31 @@
 use ascii::ToAsciiChar;
 
+#[derive(Debug)]
 enum Mode {
     ToUpper,
     ToLower,
     Punctuation,
 }
 
+static mut MODE: Mode = Mode::ToUpper;
+
 fn main() {
-    let x = 097;
-    println!("{:?}", x.to_ascii_char());
-    println!("{}", punctuation_mode(1));
     return;
 }
 
 fn uppercase_mode(code_digit: u32) -> ascii::AsciiChar {
-    // TODO: input % 27 returns a integer associated with the nth character of the alphabet
     let digit = code_digit % 27;
     let value = ToAsciiChar::to_ascii_char((digit as u8 % 27) + 64).unwrap();
     return value;
 }
 
 fn lowercase_mode(code_digit: u32) -> ascii::AsciiChar {
-    // TODO: the same as the above but with lowercase letters.
     let digit = code_digit % 27;
     let value = ToAsciiChar::to_ascii_char((digit as u8 % 27) + 96).unwrap();
     return value;
 }
 
-fn punctuation_mode(code_digit: u8) -> char {
+fn punctuation_mode(code_digit: u32) -> ascii::AsciiChar {
     // Input % 9 returns an integer associated with a symbol:
     // 1=!
     // 2=?
@@ -38,26 +36,39 @@ fn punctuation_mode(code_digit: u8) -> char {
     // 7="
     // 8='
     let input = code_digit % 9;
-    let mut output = ' ';
+    let mut output = 032;
 
     match input {
-        1 => output = '!',
-        2 => output = '?',
-        3 => output = ',',
-        4 => output = '.',
-        5 => output = ' ',
-        6 => output = ';',
-        7 => output = '"',
-        8 => output = '\'',
+        1 => output = 033,
+        2 => output = 063,
+        3 => output = 044,
+        4 => output = 046,
+        5 => output = 032,
+        6 => output = 059,
+        7 => output = 034,
+        8 => output = 039,
         _ => panic!("Failed to parse {} . . .", input),
     }
-    return output;
+
+    return ToAsciiChar::to_ascii_char(output as u8).unwrap();
 }
 
-fn decode() {
-    // TODO: main function to handle decoding a message. This function will switch on 0 to the
-    // following mode.
-    // User the Mode enum I am hoping to swap based on input.
+fn decode(input: String) -> String {
+    let mut input_vec: Vec<u32> = input
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+
+    for x in input_vec.iter() {
+        if x % 27 || x % 9 == 0 {
+            // Switch mode on 0
+        }
+        match MODE {
+            Mode::ToUpper => uppercase_mode(*x),
+            Mode::ToLower => lowercase_mode(*x),
+            Mode::Punctuation => punctuation_mode(*x),
+        }
+    }
 }
 
 fn parse_input() {
@@ -92,7 +103,11 @@ mod tests {
         assert_eq!(';', punctuation_mode(y));
     }
 
-    fn decode_test() {}
+    #[test]
+    fn decode_test() {
+        let x = "18,12312,171,763,98423,1208,216,11,500,18,241,0,32,20620,27,10".to_string();
+        assert_eq!("Right? Yes!", decode(x));
+    }
 
     fn parse_input_test() {}
 }

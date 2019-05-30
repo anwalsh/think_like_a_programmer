f119340cf0d2a2fb66553b7b49f47b7d7074c87a
Andy Walsh <andyjasonwalsh@gmail.com>
2019-05-29 21:52:09 -0500

    Implemented function to calculate the mean

--- array_mode/src/main.rs
+++ array_mode/src/main.rs
@@ -22,6 +22,10 @@ fn get_mode(input: &mut [i32]) -> i32 {
     return mode;
 }
 
+fn get_mean(input: &[i32]) -> f32 {
+    return input.iter().sum::<i32>() as f32 / (input.len() as f32);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +38,11 @@ mod tests {
             1, 1, 1, 1, 1, 2, 22, 25, 23, 90, 100, 120, 101, 101, 101, 101,
         ];
         assert_eq!(1, get_mode(&mut arr))
+    }
+
+    #[test]
+    fn find_mean_test() {
+        let mut arr: [i32; 10] = [1, 1, 2, 4, 7, 22, 22, 3, 3, 3];
+        assert_eq!(6.8, get_mean(&mut arr));
     }
 }

ad10105841ce3b30a0404dd4a534dfa99e478f1c
Andy Walsh <andyjasonwalsh@gmail.com>
2019-05-29 22:19:56 -0500

    Finished the median function

--- array_mode/src/main.rs
+++ array_mode/src/main.rs
@@ -26,13 +26,13 @@ fn get_mean(input: &[i32]) -> f32 {
     return (input.iter().sum::<i32>() as f32 / (input.len() as f32) * 1000.0).round() / 1000.0;
 }
 
-fn get_median(input: &mut [i32]) -> i32 {
+fn get_median(input: &mut [i32]) -> f32 {
     input.sort();
 
     if input.len() % 2 == 0 {
-        return (input[input.len() - 1] + input[input.len() / 2]) / 2;
+        return ((input[input.len() / 2 - 1] + input[input.len() / 2]) / 2) as f32;
     } else {
-        return input[input.len() / 2];
+        return (input[input.len() / 2]) as f32;
     }
 }
 
@@ -63,8 +63,8 @@ mod tests {
     #[test]
     fn find_median_test() {
         let mut arr: [i32; 10] = [1, 1, 2, 4, 7, 22, 22, 3, 3, 3];
-        assert_eq!(3, get_median(&mut arr));
+        assert_eq!(3.0, get_median(&mut arr));
         let mut arr: [i32; 9] = [22, 26, 1, 1, 3, 5, 102, 9, 7];
-        assert_eq!(7, get_median(&mut arr));
+        assert_eq!(7.0, get_median(&mut arr));
     }
 }
